33d5aa907c8249862eee8c08b0c9133332e65912
LRache <2746324475@qq.com>
2025-04-18 12:53:10 +0800

    feat: auto complete

--- src/App.js
+++ src/App.js
@@ -1,28 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { AutoComplete, Button } from 'antd';
+
+const DATABASE = [
+    {name: "幻舞玲珑 公孙离", type: "射手"},
+    {name: "肃归之戈 大司命", type: "战士"}
+]
+
+const NAMES = DATABASE.map(item => item.name);
 
 function App() {
     useEffect(() => {
         document.title = "王者荣耀英雄猜猜猜"
     })
+    const [guess, setGuess] = useState("");
+
+    const handleGuess = () => {
+        if (NAMES.find(name => name === guess) !== -1) {
+            
+        }
+    }
     
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+            <div>
+                <div className="title-text">王者荣耀英雄猜猜猜</div>
+                <div className="guess-box">
+                    <div className="tip-text">输入英雄中文名 / 中文称号(按回车进行猜测)：</div>
+                    <AutoComplete
+                        style={{
+                            width: "100%",
+                            fontSize: "20px",
+                        }}
+                        placeholder="请输入英雄中文名 / 中文称号，如幻舞玲珑公孙离"
+                        options={NAMES.map(name => ({value: name}))}
+                        filterOption={(inputValue, options) =>
+                            options.value.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1
+                        }
+                        onChange={(value) => {
+                            setGuess(value);
+                        }}
+                    />
+                    <Button 
+                        style={{
+                            marginTop: "15px",
+                            fontSize: "20px",
+                        }}
+                        onClick={handleGuess}
+                    >猜一猜试试看</Button>
+                </div>
+            </div>
         </div>
     );
 }
